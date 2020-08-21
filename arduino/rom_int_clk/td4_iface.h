25e061c981996f206831a7f2f6ff06f6a14a91ad
tokuhira <masatsune@tokuhira.net>
2020-08-21 23:53:03 +0900

    Update td4_iface.h
    Remove the dependency to STM32.

--- arduino/rom_int_clk/td4_iface.h
+++ arduino/rom_int_clk/td4_iface.h
@@ -4,14 +4,14 @@
 #include <stdint.h>
 
 enum {
-  RST = D2,
-  CLK = D3,
+  RST = 2,
+  CLK = 3,
 };
 
 enum { in_0 = 0, in_1, in_2, in_3, in_count };
 const uint32_t in_pins[in_count] = { A0, A1, A2, A3 };
 
 enum { out_0 = 0, out_1, out_2, out_3, out_4, out_5, out_6, out_7, out_count };
-const uint32_t out_pins[out_count] = { D4, D5, D6, D7, D8, D9, D10, D11 };
+const uint32_t out_pins[out_count] = { 4, 5, 6, 7, 8, 9, 10, 11 };
 
 #endif __td4_iface_h
