2865580d25fd5ee74f825dfa4f14aa1236acbacb
tokuhira <masatsune@tokuhira.net>
2020-08-23 00:33:01 +0900

    Create stm32cube/rom_int_clk for NUCLEO-F030R8

---
+++ stm32cube/rom_int_clk/Core/Inc/td4_rom.h
@@ -0,0 +1,30 @@
+/*
+ * td4_rom.h
+ *
+ *  Created on: Aug 22, 2020
+ *      Author: tokuhira
+ */
+
+#ifndef INC_TD4_ROM_H_
+#define INC_TD4_ROM_H_
+
+const unsigned char rom[] = {
+  0b01100000, // IN  B
+  0b10010000, // OUT B
+  0b11110000, // JMP 0000
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+  0b00000000,
+};
+
+#endif /* INC_TD4_ROM_H_ */
