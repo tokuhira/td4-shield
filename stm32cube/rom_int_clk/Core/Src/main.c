79fc392f72735d649c0c1aa99463ca79d2b38e76
tokuhira <masatsune@tokuhira.net>
2021-02-05 00:30:21 +0900

    Update codes with STM32CubeIDE
    STM32CubeIDE version: 1.4.2 -> 1.5.1
    FirmwarePackage: STM32Cube FW_F0 V1.11.0 -> STM32Cube FW_F0 V1.11.2

--- stm32cube/rom_int_clk/Core/Src/main.c
+++ stm32cube/rom_int_clk/Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under BSD 3-Clause license,
@@ -101,6 +101,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
     if (ticked)
     {
       /* Read demand address for ROM */
@@ -121,10 +124,6 @@ int main(void)
     HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, rom[address] & 1 << 5);
     HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, rom[address] & 1 << 6);
     HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, rom[address] & 1 << 7);
-
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
@@ -295,7 +294,10 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -311,7 +313,7 @@ void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- stm32cube/rom_int_clk/Core/Src/stm32f0xx_it.c
+++ stm32cube/rom_int_clk/Core/Src/stm32f0xx_it.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
   * This software component is licensed by ST under BSD 3-Clause license,
@@ -73,7 +73,9 @@ void NMI_Handler(void)
 
   /* USER CODE END NonMaskableInt_IRQn 0 */
   /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
-
+  while (1)
+  {
+  }
   /* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
